24380035035de9bc07026ebe6c88ea956b8677c5
Nelson Malbone <nhmalbone@icloud.com>
2024-05-06 19:24:36 -0400

    got the title to show up on streamlit

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ import requests
 apiKey = config.api_key
 url = f'https://api.nasa.gov/planetary/apod?api_key={apiKey}'
 response = requests.get(url)
-print(url)
+
+content = response.json()
+
+web_title = sT.title(content['title'])
