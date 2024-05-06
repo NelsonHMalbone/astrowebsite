e529d7a37c3ed8302ae29e26b4547fce3e1afec1
Nelson Malbone <nhmalbone@icloud.com>
2024-05-06 19:09:37 -0400

    api works printed out in console.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as sT
 import config
+import requests
+
+apiKey = config.api_key
+url = f'https://api.nasa.gov/planetary/apod?api_key={apiKey}'
+response = requests.get(url)
+print(url)
 
 
-# title of website
-sT.title('Welcome')
 
