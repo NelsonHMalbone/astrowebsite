df32accdd4a7977114b38b9e719c997449cfb3d7
Nelson Malbone <nhmalbone@icloud.com>
2024-05-08 16:22:28 -0400

    code for daily planet info

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import streamlit as sT
 import config
 import requests
 
+sT.set_page_config(layout='wide')
+
 apiKey = config.api_key
 url = f'https://api.nasa.gov/planetary/apod?api_key={apiKey}'
-response = requests.get(url)
 
+# first response
+response = requests.get(url)
 content = response.json()
 
-web_title = sT.title(content['title'])
+# extract url,explanation,title
+title = content['title']
+img_url = content['url']
+explanation = content['explanation']
+
+# secodary response for img download
+img_path = 'img.png'
+response2 = requests.get(img_url)
+with open(img_path, 'wb') as file:
+    file.write(response2.content)
+
+# website
+sT.title(title)
+sT.image(img_url)
+sT.write(explanation)
